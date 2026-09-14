b7d234d4e7f6b263fa83937c61440a61fce355b8
mariameneghimvieira-dev <maria.meneghim.vieira@escola.pr.gov.br>
2026-09-14 11:26:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+const botoes = document.querySelectorAll("button")
+botes.forEach(function (botao) {
+  let curtiu= false;
+  botao.addEventListener("click" , botaoClicado);
+  function botaoClicado() {
+    console.log("fui clicado");
+    let texto = botao.querySelector("span");
+    if (curtiu === false) {
+      texto.textContent++;
+      curtiu = true;
+    } else{
+      texto.textContent --;
+      curtiu = false; 
+    }
+  }
+  ))
